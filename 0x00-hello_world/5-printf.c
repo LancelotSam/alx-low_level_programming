4f7ec6e583aa9bec4dde7876379011be00edf917
Lancelot <skmburu110@gmail.com>
2023-06-15 18:57:59 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
  *main-entry point
- *a program that prints Programming is like building a multilingual puzzle
+ *a program that prints with proper grammar, but the outcome is a piece of art
  *use printf
  *Return:0(success)
  */
