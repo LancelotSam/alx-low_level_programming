6925b75e4709537a3c5aa26abbb8bf824718c414
Lancelot <skmburu110@gmail.com>
2023-06-15 19:09:54 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *main-entry point
- *a program that prints with proper grammar, but the outcome is a piece of art
+ *main- a program that prints a line multilingual puzzle
  *use printf
  *Return:0(success)
  */
 int main(void)
 {
 /*Displays the string inside quotations*/
-printf("with proper grammar, but the outcome is a piece of art,\n";
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
