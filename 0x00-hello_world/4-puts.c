4780095baaabf27387d6ad33a7181180072e67ba
Lancelot <skmburu110@gmail.com>
2023-06-15 18:20:50 -0700

    my first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
  *
  * Return:0(success)
  */
-int main(void){
-    char Mystr[]="\"Programming is like building a multilingual puzzle";
-    
-    puts(Mystr);
-    
-    return 0;
+int main(void)
+{
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
 }
