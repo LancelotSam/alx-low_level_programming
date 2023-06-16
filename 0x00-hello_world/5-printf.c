4e3e61a07e20d0c251be2d05b69490e77b5c5c5c
Lancelot <skmburu110@gmail.com>
2023-06-15 18:47:45 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main-entry point
+ *a program that prints Programming is like building a multilingual puzzle
+ *use printf
+ *Return:0(success)
+ */
+int main(void)
+{
+/*Displays the string inside quotations*/
+printf("with proper grammar\, but the outcome is a piece of art\,");
+return (0);
+}
