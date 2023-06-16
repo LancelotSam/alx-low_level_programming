f7d1a2cd90aa13c7656b68bc82e1c17ccef9462c
Lancelot <skmburu110@gmail.com>
2023-06-15 18:09:22 -0700

    my first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * a program that prints Programming is like building a multilingual puzzle
+ * use puts fn
+ *
+ * Return:0(success)
+ */
+int main(void){
+    char Mystr[]="\"Programming is like building a multilingual puzzle";
+    
+    puts(Mystr);
+    
+    return 0;
+}
