fa0728a22ec1a0f033eb37196cb1664d178d4deb
Lancelot <skmburu110@gmail.com>
2023-06-16 18:35:17 -0700

    excluding results using logical operators

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-program entry point
+ * printing alphabet in lowercase
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+/* initializing the start of the first variable */
+char alp;
+for (alp = 'a' ; alp <= 'z' ; alp++)
+if (char != e && != q)
+{
+putchar (alp);
+}
+putchar('\n');
+return (0);
+}
