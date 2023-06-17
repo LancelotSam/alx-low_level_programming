a96337975a8e41fb643fc4a8ced75bff298bbc4c
Lancelot <skmburu110@gmail.com>
2023-06-16 18:01:54 -0700

    alpHBAETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,6 +9,10 @@ int main(void)
 /* initializing the start of the first variable */
 char alp;
 for (alp = 'a' ; alp <= 'z' ; alp++)
+{
+putchar (alp);
+}
+char alp;
 for (alp = 'A' ; alp <= 'Z' ; alp++)
 {
 putchar (alp);
