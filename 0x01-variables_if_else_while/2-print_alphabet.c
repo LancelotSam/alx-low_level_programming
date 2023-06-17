3ea2646ece7da1c9be740f98f7cb02bb9e767044
Lancelot <skmburu110@gmail.com>
2023-06-16 17:32:01 -0700

    alphabets in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,11 @@ int main(void)
 {
 /* initializing the start of the first variable */
 char alphabet = 'a';
-char \n;
-while (alphabet <= z) /* using ascii values of aalphabets*/
+char alphabet = 'z';
+while (alphabet <= z)
 {
 putchar (alphabet);
 alphabet++ ; /*increment a to move to the next varibale i.e b*/
 }
-putchar(\n);
 return (0);
 }
