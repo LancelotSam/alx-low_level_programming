b8bb619757ff44e5e946ecb3f5c9c5c83d96cdcf
Lancelot <skmburu110@gmail.com>
2023-06-16 19:23:28 -0700

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-program entry point
+ * printing alphabet in lowercase and reverse
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+/* initializing the start of the first variable */
+char alp;
+for (alp = 'z' ; alp <= 'a' ; alp++)
+{
+putchar (alp);
+}
+putchar('\n');
+return (0);
+}
