4db0287590a7c40f9a8accc1d6d49fcfddb884b7
Lancelot <skmburu110@gmail.com>
2023-06-18 20:13:53 -0700

    combinations of single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main-create a program that creates a combination of,
+ * digits from 0-9
+ * Return: 0(Success)
+ */
+int main(void)/*initialize the main program*/
+{
+/*initialize the main program*/
+int i, j;
+for (i = 0; i <= 9; i++)
+for (j = 0; j <= 9; j++)
+if (i != j)
+}
+{/* check if digits are different*/
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+}
+return (0);
+}
