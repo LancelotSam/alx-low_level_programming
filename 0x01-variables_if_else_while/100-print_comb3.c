e70ad78919eef5e9cace906134c684411e839141
Lancelot <skmburu110@gmail.com>
2023-06-18 20:21:14 -0700

    combinations of two differnt numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main-create a program that creates a combination of,
+ * digits from 0-9
+ * Return: 0(Success)
+ */
+int main(void)
+{
+/*initialize the main program*/
+int i, j;
+for (i = n-1; i <= n+1; i++)
+for (j = n-1; j <= n+1; j++)
+if (i != j)
+}
+{/* check if digits are different*/
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+}
+return (0);
