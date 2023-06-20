e92373cdb6e65030f824e061e43f97a3aaa85fe0
Lancelot <skmburu110@gmail.com>
2023-06-20 15:19:37 -0700

    my header _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main-use putchar to print _putchar
- * * Return: (1) success
+ * * Return: (0) success
 */
-int _putchar(char c); /*declaring the function putchar*/
+int _putchar(); /*declaring the function putchar*/
 char main(void)
 {
 char _putchar;
 _putchar(char);
 _putchar('\n');
-Return(write(1 ,&c ,1));
+Return(0);
 }
