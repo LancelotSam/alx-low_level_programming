c3c10c89247259ff48d2aced8c27999ba8fa9047
Lancelot <skmburu110@gmail.com>
2023-06-20 14:40:46 -0700

    unistd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 /**
  * main-use putchar to print _putchar
- * * Return: (0) success
+ * * Return: (1) success
 */
-char _putchar(void); /*declaring the function putchar*/
+int _putchar(char c); /*declaring the function putchar*/
 char main(void)
 {
 char _putchar;
 _putchar(char);
 _putchar('\n');
-Return(0);
+Return(write(1 ,&c ,1));
 }
