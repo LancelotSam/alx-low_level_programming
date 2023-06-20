12ade267c53eb5865385f7d5ce3a073b84c9e5f0
Lancelot <skmburu110@gmail.com>
2023-06-20 12:09:47 -0700

    use putchar to print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-use putchar to print _putchar
+ *@Parameter: use char
+ * Return: (0) success
+ *
+ */
+void putchar(char);
+int main(void)
+{
+char [] = '_putchar';
+{
+putchar(char);
+}
+putchar('\n')i;
+Return(0);
+}
