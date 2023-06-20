e0fbdde53e4eb022f5c5abad16fc7c2d783c6627
Lancelot <skmburu110@gmail.com>
2023-06-19 17:42:35 -0700

    combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,11 @@
 int main(void)
 {
 /*initialize the main program*/
-int i, j;
+int i;
 for (i = 0; i <= 9; i++)
-for (j = 0; j <= 9; j++)
-if ( i != j)
+if ( i != i)
 {/* check if digits are different*/
 putchar(i + '0');
-putchar(j + '0');
 putchar(' ');
 }
 return (0);
