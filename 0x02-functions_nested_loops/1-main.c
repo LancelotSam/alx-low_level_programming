302336353a28e04d9b934c57039a1a8f919c47ee
Lancelot <skmburu110@gmail.com>
2023-06-21 15:24:17 -0700

    alphabet 10x

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,1 +1,11 @@
 #include "main.h"
+/**
+ * main - print_alphabet
+ * printing the alphabet in lowercase using own header
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
