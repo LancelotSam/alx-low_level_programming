1f1e43e0d1a64f63b7b2bb16ea6ab0633c71a97c
Lancelot <skmburu110@gmail.com>
2023-06-22 16:32:22 -0700

    multiplicatio of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main- entry point
+ * Function:multiply two integers
+ * Parameter 1- int a
+ * Parameter 2- int b
+ * Return: 0 (always)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
