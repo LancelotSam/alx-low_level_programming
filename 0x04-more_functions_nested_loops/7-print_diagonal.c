5dfa8334c33d79e30f7675d96437c06f29568a75
Lancelot <skmburu110@gmail.com>
2023-06-23 19:29:45 -0700

    print diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,20 +16,13 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 0; i <= n; i++)
+		int j;
 		{
 			for (j = 0; j <= n; j++)
 			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
+				_putchar('\\');
 			}
-			{
-				_putchar('\n');
-			}
+			_putchar('\n');
 		}
 	}
 }
