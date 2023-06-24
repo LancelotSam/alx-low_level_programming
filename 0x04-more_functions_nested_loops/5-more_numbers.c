b164007c56ea9ce6cfde1bd36fd7b93ce0fb96ab
Lancelot <skmburu110@gmail.com>
2023-06-23 17:43:01 -0700

    printing numbers 0-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * void more_numbers(void)-to print 5 more numbers
- * @c- 0 to 14
+ * more_numbers- this is the main function
  *
- * Return (void)
+ * To print 5 more numbers
+ * @0-14
+ * * Return (void)
  */
 void more_numbers(void)
 {
@@ -13,9 +14,13 @@ void more_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 0; c <= 14; c++)
-			_putchar(c);
+		{
+			if (c > 9)
+				_putchar('1');
+			_putchar(c % 10 + '0');
+		}
+		{
+			_putchar('\n');
+		}
 	}
-	{
-		_putchar('\n');
-	}	
 }
