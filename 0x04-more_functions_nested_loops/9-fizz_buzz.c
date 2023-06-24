0b3d2801134d12614f61422be2ab696e8885cc08
Lancelot <skmburu110@gmail.com>
2023-06-23 21:11:11 -0700

    FuzzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,14 +5,14 @@
  * main-this is the main function
  *
  * printf: prints numbers 0-100
- * @0-100: integers to be printed
+ * @1-100: integers to be printed
  * Return: 0
  */
 int main(void)
 {
 	int k;
 
-	for (k = 0; k <= 100; k++)
+	for (k = 1; k <= 100; k++)
 	{
 		if (k % 3 == 0)
 		{
@@ -25,6 +25,10 @@ int main(void)
 		else if (k % 3 == 0 && k % 5 == 0)
 		{
 			printf("FizzBuzz");
+		}
+		else if (k == 1)
+		{
+			printf("%d", k);
 		}
 		else
 		{
