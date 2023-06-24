3a41bc282426559b7d5a9549176c065ab44e8996
Lancelot <skmburu110@gmail.com>
2023-06-23 21:19:15 -0700

    FuzzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,22 +17,27 @@ int main(void)
 		if (k % 3 == 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		}
 		else if (k % 5 == 0)
 		{
 			printf("Buzz");
+			printf(" ");
 		}
 		else if (k % 3 == 0 && k % 5 == 0)
 		{
 			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if (k == 1)
 		{
 			printf("%d", k);
+			printf(" ");
 		}
 		else
 		{
 			printf("%d", k);
+			printf(" ");
 		}
 	}
 	{
