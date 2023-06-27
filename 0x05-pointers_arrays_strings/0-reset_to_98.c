c1a9fb6e3d4783f48f244378796719cb36d4f96b
Lancelot <skmburu110@gmail.com>
2023-06-26 17:13:20 -0700

    pointer to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * void reset_to_98(int *n)- this is the main function
+ *
+ * It updates the value of a parameter
+ * @Parameter: int
+ * @98: the character to be printed
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}	
