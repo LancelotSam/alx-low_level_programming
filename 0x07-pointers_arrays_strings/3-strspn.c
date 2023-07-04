e9989efd188e1b790ba7896cf3e59297daf1d1c7
Lancelot <skmburu110@gmail.com>
2023-07-04 13:45:41 -0700

    pre-string length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,17 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i;
 	unsigned int j;
+	unsigned int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
+				count++;
+			}
+		if (accept[j]+1 == '\0')
 			{
 				return (s[i]);
 			}
