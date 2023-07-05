eec8881ae4e3df11fd327f826f984db7532f1603
Lancelot <skmburu110@gmail.com>
2023-07-04 19:19:10 -0700

    refeferencing double pointer to pointer to a different location

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_string-this is the main function
+ *
+ * It sets the value of a pointer to char
+ *@s:the value of a pointer
+ *@to: the value to be changed to
+ * Return:void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
