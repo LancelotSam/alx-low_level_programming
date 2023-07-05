ca0c8ba5652841a268e13f51d6646cc0d8cf463e
Lancelot <skmburu110@gmail.com>
2023-07-05 16:18:33 -0700

    factorial of n including < 0, = 0 and else using recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial-this is the main function
+ *
+ * It prints the factorial of a number
+ * @n:the integer whose factorial is to be printed
+ * Return: factorial integer
+ */
+int factorial(int n)
+{
+	if(n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
