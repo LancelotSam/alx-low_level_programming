19ed7ef147f682de798ab02c308e06e35b0e11c0
Lancelot <skmburu110@gmail.com>
2023-07-05 16:37:00 -0700

    x to power y, by decrementing y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion-this is the main function
+ *
+ * It prints the value of raised to power
+ * @x: the base integer
+ * @y: the power integer
+ * Return: an integer of x raised to y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
