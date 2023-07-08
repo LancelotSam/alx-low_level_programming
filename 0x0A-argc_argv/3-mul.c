e5af154ccb29f3948266b676821b784b337b4601
Lancelot <skmburu110@gmail.com>
2023-07-08 15:59:11 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,20 +10,19 @@
 int main(int argc, char *argv[])
 {
 	int k = 1;
-	int z = 1;
+	int z = 0;
 	char E[] = "Error";
 
 	int i, j;
 
 	printf("Enter two numbers: ");
-	scanf("%d %d", &i, &j);
 
 	if (scanf("%d %d", &i, &j) != 2)
 	{
 		printf("%s\n", E);
 		return (1);
 	}
-	for (z = 1; z < argc; z++)/*start from 1 since 0 is the name of program*/
+	for (z = 0; z < argc; z++)/*start from 1 since 0 is the name of program*/
 	{/* k * arvg[1] is wrong, integer vs pointer*/
 		k *= strtol(argv[z], NULL, 10);
 	}
