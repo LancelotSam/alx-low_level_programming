c572fda3c3d8bb0c23a7f1fd203d11f29e5e1aa5
Lancelot <skmburu110@gmail.com>
2023-07-07 17:48:30 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-this is the main function
+ *
+ * It prints the name of the program
+ * @argc: the number of arguments
+ * @argv: the vector array of the commands to be used
+ * Return: 0 if program runs successfully
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("My name is: %s\n", argv[0]);
+
+	return (0);
+}
