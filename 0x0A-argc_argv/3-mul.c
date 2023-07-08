52197c56fd0a03344e0fd31181a81fdbfe54a5f8
Lancelot <skmburu110@gmail.com>
2023-07-08 16:32:57 -0700

    multiplication, string to integer conversion

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,23 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int k = 1;
+	int result = 1;
 	int z = 0;
 	char E[] = "Error";
 
 	int i, j;
 
-	printf("Enter two numbers: ");
+	printf("Enter two numbers:");
 
 	if (scanf("%d %d", &i, &j) != 2)
 	{
 		printf("%s\n", E);
 		return (1);
 	}
-	for (z = 0; z < argc; z++)/*start from 1 since 0 is the name of program*/
+	for (z = 1; z < argc; z++)/*start from 1 since 0 is the name of program*/
 	{/* k * arvg[1] is wrong, integer vs pointer*/
-		k *= strtol(argv[z], NULL, 10);
+		result *= _atoi(argv[z]);
 	}
-	printf("%d\n", k);
+	printf("%d\n", result);
 	return (0);
 }
