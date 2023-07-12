fdebad6878a8ab3976ca3ae69d9bffec6e32218e
Lancelot <skmburu110@gmail.com>
2023-07-11 19:21:32 -0700

    grid -2D array, rows(h) & columns[h]

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * **alloc_grid-this is the main function
+ *
+ * It returns a pointer to a 2 dimensional array of int
+ * @width: the widthe of the array
+ * @height: the height of the array
+ * Return: NULL if width or height is 0 or negative
+ */
+int **alloc_grid(int width, int height)
+{
+	int h = 0;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(*grid) * height);
+	if (grid == NULL)/*memry allocation for all rows *height*/
+	{
+		return (NULL);
+	}
+	for (h = 0; h < height; h++)
+	{
+		grid[h] = malloc(sizeof(**grid) * width);
+		if(grid[h] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (grid);
+}
