f7c6a5a781cb450519e6b171618ea903a93d5842
Lancelot <skmburu110@gmail.com>
2023-07-13 18:26:35 -0700

    allocate b bytes of memeory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *malloc_checked-this is the main function
+ *
+ * It allocates memory using malloc
+ * @b:the unsigned integer to be used as a return
+ * Return: A pointer to the allocated memory or 98 on failure
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *i;
+
+	i = malloc(b);
+
+	if (i == NULL)
+	{
+		exit (98);
+	}
+	return (i);
+}
