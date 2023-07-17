27457baab288300c7d09c2b3c6da1cea601de19c
Lancelot <skmburu110@gmail.com>
2023-07-17 15:52:04 -0700

    revised chnages in definition of header from main to respective

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-#include <stdio.h>
 #define SIZE 1024
 
 #endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  this is the main function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    
+    return (0);
+}
