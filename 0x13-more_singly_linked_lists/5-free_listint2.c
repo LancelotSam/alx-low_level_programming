8e3508e05dd534dedd7a0affc97019b4e78bbebe
Lancelot <skmburu110@gmail.com>
2023-08-01 07:27:49 -0700

    free a list pointed to by a pointer to a pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_listint2-this is the main function
+ * 
+ * it frees list and sets head to NULL
+ * @head:pointer of a pointer to the the head of the list
+ * Return:void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+	/**
+	 * head = (*head)->next;
+	 * head->next = NULL;
+	 * */
+}
