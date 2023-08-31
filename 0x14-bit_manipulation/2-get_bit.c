696baee555ffa916b7becfea568d00f223c28f3d
Lancelot <skmburu110@gmail.com>
2023-08-31 15:03:22 -0700

    return bit at 8th index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * get_bit-this is the main function
+ *
+ * It returns the bit value at any index
+ * @n:the value
+ * @index:the index who bit value is to be returned
+ * Return: bit value for index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int size;
+	unsigned long int bitmask;
+
+	size = sizeof(unsigned long int) * 8;
+	bitmask = 1ul << index;
+	/*1 << (size - 1)*/
+
+	if (index >= size)
+		return (-1);
+
+	return ((n & bitmask) != 0);
+}
+
+
