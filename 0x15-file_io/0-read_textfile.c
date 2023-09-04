f35d02608a085625351ae81b19f1faf1d03fc7e7
Lancelot <skmburu110@gmail.com>
2023-09-04 16:30:22 -0700

    read a textfile using fd

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * read_textfile-this is the main function
+ *
+ * It reads a textfile
+ * @filename: a pointer to the name of the file
+ * @letters: the number of letters in the file
+ * Return: the number of read letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, read_let, Written;
+
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd  == -1)
+		return (0);
+	read_let = read(fd, buffer, letters);
+
+	Written = write(STDOUT_FILENO, buffer, read_let);
+
+	close(fd);
+
+	free(buffer);
+
+	return (Written);
+}
