da008b6228c036cdd0fe8a3ea3dc44f5a42c3491
Lancelot <skmburu110@gmail.com>
2023-09-22 18:15:22 -0700

    tried to fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,21 +27,26 @@ int _strlen(char *str)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t txt_cont_len;
-
-	int fd, Appended;
+	int fd, appended;
 
 	if (filename == NULL)
 	{
+		fprintf(stderr, "Filename is NULL\n");
 		return (-1);
 	}
+
 	if (access(filename, F_OK) == -1)
 	{
+		fprintf(stderr, "File does not exist\n");
 		return (-1);
 	}
+
 	if (text_content == NULL)
 	{
-		return (1);
+		fprintf(stderr, "Text content is NULL\n");
+		return (-1);
 	}
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
@@ -49,15 +54,57 @@ int append_text_to_file(const char *filename, char *text_content)
 		perror("Error while opening the file");
 		return (-1);
 	}
+
 	txt_cont_len = _strlen(text_content);
 
-	Appended = write(fd, text_content, txt_cont_len);
-	if (Appended == -1)
+	appended = write(fd, text_content, txt_cont_len);
+
+	if (appended == -1)
 	{
 		perror("Error Appending");
 		close(fd);
 		return (-1);
 	}
+
 	close(fd);
-	return (1);
+
+	return (0);
 }
+
+/*int append_text_to_file(const char *filename, char *text_content)
+  {
+  ssize_t txt_cont_len;
+
+  int fd, Appended;
+
+  if (filename == NULL)
+  {
+  return (-1);
+  }
+  if (access(filename, F_OK) == -1)
+  {
+  return (-1);
+  }
+  if (text_content == NULL)
+  {
+  return (1);
+  }
+  fd = open(filename, O_WRONLY | O_APPEND);
+
+  if (fd == -1)
+  {
+  perror("Error while opening the file");
+  return (-1);
+  }
+  txt_cont_len = _strlen(text_content);
+
+  Appended = write(fd, text_content, txt_cont_len);
+  if (Appended == -1)
+  {
+  perror("Error Appending");
+  close(fd);
+  return (-1);
+  }
+  close(fd);
+  return (1);
+  }*/
