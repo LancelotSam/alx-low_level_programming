4d526c454502abad331edb6a029e25b444556a3a
Lancelot <skmburu110@gmail.com>
2023-09-22 23:07:15 -0700

    print binary rectified

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,20 +8,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int bitmask = 1;
-	int size = sizeof(unsigned long int) * 8;/*1 << 31 is for a spec systems*/
+	unsigned long int temp;
+	int shift;
 
-	bitmask = 1 << (size - 1);/*initialize bitmask to msb*/
-
-	while (bitmask != 0)
+	if (n == 0)
 	{
-		if ((n & bitmask) != 0)/*if bit is set (1) and not clear(0)*/
-			_putchar('1');
-		else
-			_putchar('0');
-		bitmask >>= 1;/*move bitmask to next bit*/
+		printf("0");
+		return;
 	}
-	if (n == 0)/*if number is 0*/
-		_putchar('0');
-	_putchar('\n');
+
+	for (temp = n, shift = 0; (temp >>= 1) > 0; shift++)
+		;
+
+	for (; shift >= 0; shift--)
+	{
+		if ((n >> shift) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
 }
